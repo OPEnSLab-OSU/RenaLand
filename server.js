649cab74cb003d183475f074bb9c4d5a2fa22f69
Samuel Jadzak <Jadzak60636@gmail.com>
2020-12-11 00:19:15 -0800

    html and handlebars fixes

--- server.js
+++ server.js
@@ -6,19 +6,22 @@ var videoPostData = require('./videoPostData.json');
 
 var app = express();
 var port = process.env.PORT || 8000;
+app.use(express.static('public'));
 
 app.engine('handlebars', exphbs({ defaultLayout: 'completeTemplate'}));
 app.set('view engine', 'handlebars');
-app.use(express.static('public'));
+
 
 //renders main page for renaLand
 app.get('/', function(req, res, next){
-    res.status(200).render('mainPageTemplate');
+    res.status(200).render('mainPageTemplate',{videoPosts: videoPostData});
   });
 
-app.get('/', function(req, res, next){
-    res.status(200).render('mainPageTemplate',{videoPosts: postData});
+
+app.get('*', function (req, res, next) {
+  res.status(404).render('404Template.handlebars');
   });
+  
 
 app.listen(port, function () {
     console.log("== Server is listening on port", port);
