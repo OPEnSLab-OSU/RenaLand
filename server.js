edf654a0e34c450802c44f6bf3bc0768803c9eb6
Samuel Jadzak <Jadzak60636@gmail.com>
2020-12-10 19:38:23 -0800

    sam pushing changes

--- server.js
+++ server.js
@@ -2,18 +2,23 @@
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+var videoPostData = require('./videoPostData.json');
 
 var app = express();
 var port = process.env.PORT || 8000;
 
-app.engine('handlebars', exphbs({ defaultLayout: null}));
+app.engine('handlebars', exphbs({ defaultLayout: 'completeTemplate'}));
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
+//renders main page for renaLand
+app.get('/', function(req, res, next){
+    res.status(200).render('mainPageTemplate');
+  });
 
-
-
-
+app.get('/', function(req, res, next){
+    res.status(200).render('mainPageTemplate',{videoPosts: postData});
+  });
 
 app.listen(port, function () {
     console.log("== Server is listening on port", port);
